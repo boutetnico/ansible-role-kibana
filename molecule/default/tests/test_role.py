6a336d82937d9add59664a462500cb1b028843c5
Nicolas Boutet <amd3002@gmail.com>
2026-02-11 10:34:31 +0100

    Remove legacy apt-transport-https

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -4,7 +4,7 @@ PKG = "kibana"
 SERVICE = "kibana"
 
 
-@pytest.mark.parametrize("name", ["apt-transport-https", "gnupg", "python3-debian"])
+@pytest.mark.parametrize("name", ["gnupg", "python3-debian"])
 def test_dependencies_are_installed(host, name):
     package = host.package(name)
     assert package.is_installed
